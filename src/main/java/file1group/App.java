164159277dd682f76b2a4eee4b6d433e0c379082
venkatesh <venkatesh.sap2014@gmail.com>
2022-03-04 20:32:01 +0530

    maven

---
+++ src/main/java/file1group/App.java
@@ -0,0 +1,13 @@
+package file1group;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
